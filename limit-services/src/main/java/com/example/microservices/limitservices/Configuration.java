09296439c4342cd58715bdffd63aefe41be73be0
iamashishbisht <58081351+iamashishbisht@users.noreply.github.com>
2021-05-04 19:09:48 +0530

    Add files via upload

---
+++ limit-services/src/main/java/com/example/microservices/limitservices/Configuration.java
@@ -0,0 +1,36 @@
+package com.example.microservices.limitservices;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")  //prefix limit-service that is used before minimum and maximum
+public class Configuration {
+	
+	
+	private int minimum;
+	
+	private int maximum;
+	
+	
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+	
+	
+	
+
+}
